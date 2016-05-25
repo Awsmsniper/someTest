f23dd933339dd230e15a4fa7ebfa7eb6c4b6ea5a
Zhao Guangjian <zgj0315@gmail.com>
2016-05-25 09:37:55 +0800

    json client create

---
+++ JSONClient/src/main/java/com/qzt360/service/JSONClient.java
@@ -0,0 +1,45 @@
+package com.qzt360.service;
+
+import java.io.IOException;
+
+import org.apache.http.HttpEntity;
+import org.apache.http.client.ClientProtocolException;
+import org.apache.http.client.config.RequestConfig;
+import org.apache.http.client.methods.CloseableHttpResponse;
+import org.apache.http.client.methods.HttpPost;
+import org.apache.http.entity.StringEntity;
+import org.apache.http.impl.client.CloseableHttpClient;
+import org.apache.http.impl.client.HttpClientBuilder;
+import org.apache.http.util.EntityUtils;
+
+import lombok.extern.java.Log;
+
+@Log
+public class JSONClient {
+	public void getSS() {
+		HttpClientBuilder httpClientBuilder = HttpClientBuilder.create();
+		CloseableHttpClient closeableHttpClient = httpClientBuilder.build();
+		RequestConfig config = RequestConfig.custom().build();
+		HttpPost httpPost = new HttpPost("http://localhost:8080/ss");
+		httpPost.setConfig(config);
+		StringEntity entity = new StringEntity(
+				"{\"chname\":null,\"operator\":null,\"status\":null,\"page\":1,\"count\":1000}", "utf-8");
+		entity.setContentType("application/json");
+		httpPost.setEntity(entity);
+		String str = "";
+		try {
+			CloseableHttpResponse response = closeableHttpClient.execute(httpPost);
+			HttpEntity httpEntity = response.getEntity();
+			if (httpEntity != null) {
+				str = EntityUtils.toString(httpEntity, "UTF-8");
+			}
+		} catch (ClientProtocolException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		log.info(str);
+	}
+}
---
+++ JSONClient/src/test/java/com/qzt360/service/JSONClientTest.java
@@ -0,0 +1,12 @@
+package com.qzt360.service;
+
+import junit.framework.TestCase;
+
+public class JSONClientTest extends TestCase {
+
+	public void testGetSS() {
+		JSONClient jsonC = new JSONClient();
+		jsonC.getSS();
+	}
+
+}
