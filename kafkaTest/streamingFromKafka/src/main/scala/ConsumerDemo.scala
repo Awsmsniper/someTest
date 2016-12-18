03c8f771358c5034601e6b8e4eff714e62f86a6a
Zhao Guangjian <zgj0315@gmail.com>
2016-12-18 12:37:31 +0800

    streaming kafka can work

--- kafkaTest/streamingFromKafka/src/main/scala/ConsumerDemo.scala
+++ kafkaTest/streamingFromKafka/src/main/scala/ConsumerDemo.scala
@@ -1,5 +1,6 @@
 import org.apache.spark.SparkConf
-import org.apache.spark.streaming.{Seconds, StreamingContext}
+import org.apache.spark.streaming.kafka.KafkaUtils
+import org.apache.spark.streaming.{Minutes, Seconds, StreamingContext}
 
 /**
   * Created by zhaogj on 18/12/2016.
@@ -8,10 +9,24 @@ object ConsumerDemo {
   def main(args: Array[String]): Unit = {
     val conf = new SparkConf().setMaster("local[2]").setAppName("NetworkWordCount")
     val ssc = new StreamingContext(conf, Seconds(10))
+    val zkQuorum = "localhost:2181"
+    val group = "com.qzt360"
+    val topics = "the-topic"
+    val numThreads = 1
 
+    ssc.checkpoint("checkpoint")
 
+    val topicMap = topics.split(",").map((_, numThreads.toInt)).toMap
+    val lines = KafkaUtils.createStream(ssc, zkQuorum, group, topicMap).map(_._2)
+    val words = lines.flatMap(_.split(" "))
+    words.print()
+//    val wordCounts = words.map(x => (x, 1L))
+//      .reduceByKeyAndWindow(_ + _, _ - _, Minutes(2), Seconds(2), 2)
+//    wordCounts.print()
 
     ssc.start()
     ssc.awaitTermination()
+
+
   }
 }
